4ac38a936d5da05300d8bcea5b43426cff1b5675
Douglas Dias Leal <douglas_san@hotmail.com>
2025-05-31 18:31:45 +0900

    feat: add patients page

---
+++ src/app/(protected)/patients/page.tsx
@@ -0,0 +1,29 @@
+import { eq } from 'drizzle-orm'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { db } from '@/db'
+import { usersToClinicsTable } from '@/db/schema'
+import { auth } from '@/lib/auth'
+
+const PatientsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+  if (!session?.user) {
+    redirect('/authentication')
+  }
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  })
+  if (clinics.length === 0) {
+    redirect('/clinic-form')
+  }
+  return (
+    <div>
+      <h1>Pacientes</h1>
+    </div>
+  )
+}
+
+export default PatientsPage
